81314acfbd3d4e4119852e226043ea1704c42894
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-10-02 16:23:28 +0600

    start problem solving

---
+++ output/script.js
@@ -0,0 +1,13 @@
+"use strict";
+/* ________________________________ Problem - 1 ________________________________
+Chef has AA patties and BB buns. To make 11 burger, Chef needs 11 patty and 11 bun.
+Find the maximum number of burgers that Chef can make.
+*/
+let totalBurger;
+const maxBurger = (a, b) => {
+    return (totalBurger = a < b ? a : b);
+};
+/* ________________________________ Problem - 2 ________________________________
+Chef has AA patties and BB buns. To make 11 burger, Chef needs 11 patty and 11 bun.
+Find the maximum number of burgers that Chef can make.
+*/ 
