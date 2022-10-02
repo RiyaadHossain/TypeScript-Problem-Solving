cf4679ed9211531e6963a72525dc4a9e9075fc48
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-10-02 21:22:02 +0600

    all problem solved expect one

--- output/script.js
+++ output/script.js
@@ -39,10 +39,10 @@ Link: https://www.codechef.com/submit/PRIZEPOOL
 */
 let targetAchieved = 0;
 const practiceChecking = (w1, w2, w3, w4) => {
-    w1 >= 10 ? targetAchieved += 1 : null;
-    w2 >= 10 ? targetAchieved += 1 : null;
-    w3 >= 10 ? targetAchieved += 1 : null;
-    w4 >= 10 ? targetAchieved += 1 : null;
+    w1 >= 10 ? (targetAchieved += 1) : null;
+    w2 >= 10 ? (targetAchieved += 1) : null;
+    w3 >= 10 ? (targetAchieved += 1) : null;
+    w4 >= 10 ? (targetAchieved += 1) : null;
     return targetAchieved;
 };
 // console.log(practiceChecking(10, 9, 13, 32));
@@ -55,7 +55,7 @@ const findPretty = (a, b) => {
     for (let n = a; n <= b; n++) {
         numberArray.push(n);
     }
-    numberArray.forEach(number => {
+    numberArray.forEach((number) => {
         const lastDigit = String(number).slice(-1);
         if (lastDigit === "2" || lastDigit === "3" || lastDigit === "9") {
             prettyNumber += 1;
@@ -66,4 +66,31 @@ const findPretty = (a, b) => {
 console.log(findPretty(1, 10));
 /* ________________________________ Problem - 7 ________________________________
 Link: https://www.codechef.com/submit/DOLL
-*/ 
+*/
+/* ________________________________ Problem - 8 ________________________________
+Link: https://www.codechef.com/submit/MANIPULATE
+*/
+let EzioWin;
+const manipulate = (ability, guard) => {
+    EzioWin = ability > guard ? "YES" : "NO";
+    return EzioWin;
+};
+/* ________________________________ Problem - 9 ________________________________
+Link: https://www.codechef.com/submit/BATTERYLOW
+*/
+let batteryLow;
+const showNotification = (charge) => {
+    batteryLow = charge <= 15 ? "Yes" : "No";
+    return batteryLow;
+};
+/* ________________________________ Problem - 10 ________________________________
+Link: https://www.codechef.com/submit/BATTERYLOW
+*/
+let isPossible;
+const checkPossibility = (teamFirst1, teamSecond1, teamFirst2, teamSecond2) => {
+    isPossible =
+        teamFirst1 <= teamFirst2 && teamSecond1 <= teamSecond2
+            ? "POSSIBLE"
+            : "IMPOSSIBLE";
+    return isPossible;
+};
--- src/script.ts
+++ src/script.ts
@@ -51,12 +51,12 @@ Link: https://www.codechef.com/submit/PRIZEPOOL
 let targetAchieved: number = 0;
 
 const practiceChecking = (w1: number, w2: number, w3: number, w4: number) => {
-  w1 >= 10 ? targetAchieved += 1 : null
-  w2 >= 10 ? targetAchieved += 1 : null
-  w3 >= 10 ? targetAchieved += 1 : null
-  w4 >= 10 ? targetAchieved += 1 : null
+  w1 >= 10 ? (targetAchieved += 1) : null;
+  w2 >= 10 ? (targetAchieved += 1) : null;
+  w3 >= 10 ? (targetAchieved += 1) : null;
+  w4 >= 10 ? (targetAchieved += 1) : null;
 
-  return targetAchieved
+  return targetAchieved;
 };
 
 // console.log(practiceChecking(10, 9, 13, 32));
@@ -67,23 +67,60 @@ Link: https://www.codechef.com/submit/PRIZEPOOL
 let prettyNumber: number = 0;
 const findPretty = (a: number, b: number): number => {
   const numberArray: number[] = [];
-  for (let n = a; n <= b; n++){
-    numberArray.push(n)
+  for (let n = a; n <= b; n++) {
+    numberArray.push(n);
   }
 
-  numberArray.forEach(number => {
-    const lastDigit = String(number).slice(-1)
-    if ( lastDigit === "2" || lastDigit === "3" || lastDigit === "9" ) {
-      prettyNumber += 1
+  numberArray.forEach((number) => {
+    const lastDigit = String(number).slice(-1);
+    if (lastDigit === "2" || lastDigit === "3" || lastDigit === "9") {
+      prettyNumber += 1;
     }
-  })
+  });
 
-  return prettyNumber
-
-}
+  return prettyNumber;
+};
 
 console.log(findPretty(1, 10));
 
 /* ________________________________ Problem - 7 ________________________________ 
 Link: https://www.codechef.com/submit/DOLL
 */
+
+/* ________________________________ Problem - 8 ________________________________ 
+Link: https://www.codechef.com/submit/MANIPULATE
+*/
+
+let EzioWin: string;
+const manipulate = (ability: number, guard: number): string => {
+  EzioWin = ability > guard ? "YES" : "NO";
+  return EzioWin;
+};
+
+/* ________________________________ Problem - 9 ________________________________ 
+Link: https://www.codechef.com/submit/BATTERYLOW
+*/
+
+let batteryLow: string;
+const showNotification = (charge: number): string => {
+  batteryLow = charge <= 15 ? "Yes" : "No";
+  return batteryLow;
+};
+
+/* ________________________________ Problem - 10 ________________________________ 
+Link: https://www.codechef.com/submit/BATTERYLOW
+*/
+
+let isPossible: string;
+const checkPossibility = (
+  teamFirst1: number,
+  teamSecond1: number,
+  teamFirst2: number,
+  teamSecond2: number
+): string => {
+  isPossible =
+    teamFirst1 <= teamFirst2 && teamSecond1 <= teamSecond2
+      ? "POSSIBLE"
+      : "IMPOSSIBLE";
+  return isPossible;
+};
